16425972875afff1431ac20b271b43ecfabfc0ec
harsh157 <harsh12213@gmail.com>
2015-04-19 15:50:11 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,120 @@
+/*
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+*/
+
+
+
+
+
+var map;
+var marker;
+var watchID;
+
+$(document).ready(function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+    //uncomment for testing in Chrome browser
+//    onDeviceReady();
+});
+
+function onDeviceReady() {
+    $(window).unbind();
+    $(window).bind('pageshow resize orientationchange', function(e) {
+        max_height();
+    });
+    max_height();
+    google.load("maps", "3.8", {"callback": map, other_params: "sensor=true&language=en"});
+}
+
+function max_height() {
+    var h = $('div[data-role="header"]').outerHeight(true);
+    var f = $('div[data-role="footer"]').outerHeight(true);
+    var w = $(window).height();
+    var c = $('div[data-role="content"]');
+    var c_h = c.height();
+    var c_oh = c.outerHeight(true);
+    var c_new = w - h - f - c_oh + c_h;
+    var total = h + f + c_oh;
+    if (c_h < c.get(0).scrollHeight) {
+        c.height(c.get(0).scrollHeight);
+    } else {
+        c.height(c_new);
+    }
+}
+
+function map() {
+    var latlng = new google.maps.LatLng(28.50, 77.02);
+    var myOptions = {
+        zoom: 15,
+        center: latlng,
+        streetViewControl: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: true
+    };
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+    google.maps.event.addListenerOnce(map, 'tilesloaded', function() {
+        watchID = navigator.geolocation.watchPosition(gotPosition, null, {maximumAge: 5000, timeout: 60000, enableHighAccuracy: true});
+    });
+}
+
+// Method to open the About dialog
+function showAbout() {
+    showAlert("Google Maps", "Created with NetBeans 7.4");
+}
+;
+
+function showAlert(message, title) {
+    if (window.navigator.notification) {
+        window.navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+}
+
+function gotPosition(position) {
+    map.setCenter(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+
+    var point = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    if (!marker) {
+        //create marker
+        marker = new google.maps.Marker({
+            position: point,
+            map: map
+        });
+    } else {
+        //move marker to new position
+        marker.setPosition(point);
+    }
+}
